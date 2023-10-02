ebfce15e85f3cae50777e7b1c7e3ba539513334f
SebastianMancilla <gonzalezsebas43@gmail.com>
2023-10-02 12:07:31 -0600

    add dist/

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  API_KEY: '',
-  BASE_URL: '',
-  IMG_URL: '',
-  YT_URL: '',
+  API_KEY: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTcwMDNkZTExYjg1YjQ5MTRlYzkzNTQ5YmM1NWMwYSIsInN1YiI6IjVkY2VlM2FhOGE4NGQyMDAxNDY5OTI0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IRwet0jg-FfvIjgArTwOhGJmwDGgGEn_WWbOdPuB12A',
+  BASE_URL: 'https://api.themoviedb.org/3',
+  IMG_URL: 'https://image.tmdb.org/t/p/w',
+  YT_URL: 'https://www.youtube.com/embed/',
 };
